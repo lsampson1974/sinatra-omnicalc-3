e553d8fa0569ef719304f54023af9afbf22f2bfb
Lamont Sampson <148111503+lsampson1974@users.noreply.github.com>
2024-01-16 10:00:07 +0000

    Updating code for layout and umbrella

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require "http"
+require "json"
+require "uri"
+
 #We will take a crack at building this :
 
 get("/") do
-  
+  redirect("/umbrella")
+end
+
+get("/umbrella") do
+
 end
