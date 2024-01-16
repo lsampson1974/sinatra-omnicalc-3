7ed64be18a4b29fd5ca04c050a898174866d5e2d
Lamont Sampson <148111503+lsampson1974@users.noreply.github.com>
2024-01-16 11:39:57 +0000

    completed the umbrella part, now starting the gtp single message

--- app.rb
+++ app.rb
@@ -18,11 +18,15 @@ get("/umbrella") do
   erb(:umbrella)
 end
 
-get("/process_umbrella") do
+post("/process_umbrella") do
+
+  @user_location = params.fetch("user_location")
 
   uri_encoded_location = URI.encode_uri_component(@user_location)
 
   google_maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{uri_encoded_location}&key=#{google_maps_key}"
+
+  parsed_map_data = JSON.parse(HTTP.get(google_maps_url), object_class: OpenStruct)
 
   @location_latitude = parsed_map_data.results[0].geometry.location.lat
   @location_longitude = parsed_map_data.results[0].geometry.location.lng
@@ -33,20 +37,24 @@ get("/process_umbrella") do
 
   puts weather_data
   
-  #@current_temperature = weather_data.currently.temperature
+  @current_temperature = weather_data.currently.temperature
 
-  #percentagePrecipProbability = weather_data.hourly.data[0].precipProbability*100
+  percentagePrecipProbability = weather_data.hourly.data[0].precipProbability*100
 
-  #if percentagePrecipProbability >= 10.0
-  #  @umbrella_message = "You should take an umbrella."
+  if percentagePrecipProbability >= 10.0
+    @umbrella_message = "You should take an umbrella."
   
-  #else 
-  #  @umbrella_message = "You probably won't need an umbrella."
+  else 
+    @umbrella_message = "You probably won't need an umbrella."
   
-  #end
+  end
   
-  #@precip_type = weather_data.hourly.data[hours].precipType
+  @summary = weather_data.currently.summary
 
+  erb(:umbrella_result)
 
+end
 
+get ("/gtp_single_message") do
+  erb(:gtp_single_message)
 end
