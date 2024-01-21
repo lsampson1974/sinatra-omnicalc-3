44032a7c9e7a072fd4b24dac94b7db0756ae230f
Lamont Sampson <148111503+lsampson1974@users.noreply.github.com>
2024-01-21 12:39:15 +0000

    Finishing touches to app

--- app.rb
+++ app.rb
@@ -10,57 +10,114 @@ require "sqlite3"
 
 google_maps_key = ENV.fetch("GMAPS_KEY")
 pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
-open_ai_api_key = ENV.fetch("OPEN_AI_KEY")
 
-# Let's setup our chat history DB :
 
-begin
+#===========================================================
+# Let's start off with a fresh DB or reset when the user 
+# wants to clear the DB:
+
+def set_reset_db
+
+  begin
     
-  db = SQLite3::Database.open "chat_history.db"
+    db = SQLite3::Database.open "chat_history.db"
 
-  setups = [
-    'drop table if exists chat_history',
-    'create table chat_history(username text, message text)'
-  ]
+    setups = [
+      'drop table if exists chat_history',
+      'create table chat_history(username text, message text)'
+    ]
 
-  for action in setups
-    db.execute action
+    for action in setups
+      db.execute action
+    end
+  
+  rescue SQLite3::Exception => excp  
+    puts "Issue with DB : #{excp} "
+  
+  ensure
+    db.close if db
+  end
+
+end
+
+#==========================================================
+#Here's our function for responding to user messages to the 
+# ChatGPT AI.  Let's have 2 modes : regular response or 
+# "Shakespere" response.
+
+def chat_response(user_message, mode = "regular")
+
+  open_ai_api_key = ENV.fetch("OPEN_AI_KEY")
+
+  request_headers_hash = {
+    "Authorization" => "Bearer #{open_ai_api_key}",
+    "content-type" => "application/json"}
+
+  if mode == "shakespere" 
+
+      request_body_hash = {
+        "model" => "gpt-3.5-turbo",
+        "messages" => [
+        {
+          "role" => "system",
+          "content" => "You are a helpful assistant who talks like Shakespeare."
+        },
+        {
+          "role" => "user",
+          "content" => "#{user_message}"
+        }
+       ]
+      }
+   
+  else
+
+    request_body_hash = {
+      "model" => "gpt-3.5-turbo",
+      "messages" => [
+      {
+        "role" => "user",
+        "content" => "#{user_message}"
+      }
+     ]
+    }
+  
   end
 
 
+  request_body_json = JSON.generate(request_body_hash)
 
-#puts "\nSelecting from both tables - ALL possible records"
-#db.execute( "select * from chat_history" ) do |row|
-#    puts row
-#end
+  raw_response = HTTP.headers(request_headers_hash).post(
+        "https://api.openai.com/v1/chat/completions",
+        :body => request_body_json
+  ).to_s
 
+  parsed_response = JSON.parse(raw_response, object_class: OpenStruct)
 
-    #'insert into chat_history values ("someuser","Hello","202401190527")'
-    #'insert into chat_history values ("computeuser", "Hi how are you ?","202401190528")'
+  return parsed_response.choices[0].message.content   
 
-  
-rescue SQLite3::Exception => excp  
-  puts "Issue with DB : #{excp} "
-  
-ensure
-  db.close if db
-end
-
-
-
-
-get("/") do
-  redirect("/umbrella")
-end
+end # Of method
 
 
 #==========================================================
+#==========================================================
+# Let's welcome the user to our application :
+
+get("/") do
+
+  set_reset_db()
+  erb(:welcome_message)
+
+end
+
+#==========================================================
+# Our first function is the classic "umbrella" application : 
 
 get("/umbrella") do
   erb(:umbrella)
 end
 
-#------------------
+
+#---------------------------------------------
 
 post("/process_umbrella") do
 
@@ -98,61 +155,46 @@ post("/process_umbrella") do
 end
 
 #==========================================================
+# Next. We have a single message response from ChatGPT to the
+# user.  Here we'll set ChatGPT to send a "Shakespere" response :
 
 get ("/message") do
   erb(:gtp_single_message)
 end
 
-#------------------
+#---------------------------------------------
 
 post ("/process_single_message") do
 
-  @user_message = params.fetch("the_message")
+  the_message = params.fetch("the_message")
 
-  request_headers_hash = {
-  "Authorization" => "Bearer #{open_ai_api_key}",
-  "content-type" => "application/json"}
-
-  request_body_hash = {
-    "model" => "gpt-3.5-turbo",
-    "messages" => [
-      {
-        "role" => "system",
-        "content" => "You are a helpful assistant who talks like Shakespeare."
-      },
-      {
-        "role" => "user",
-        "content" => "#{@user_message}"
-      }
-    ]
-  }
-
-  request_body_json = JSON.generate(request_body_hash)
-
-  raw_response = HTTP.headers(request_headers_hash).post(
-    "https://api.openai.com/v1/chat/completions",
-    :body => request_body_json
-  ).to_s
-
-  parsed_response = JSON.parse(raw_response, object_class: OpenStruct)
-
-  @response_message = parsed_response.choices[0].message.content
-
+  @response_message = chat_response(the_message, "shakespere")
   erb(:gtp_response)
   
 end
 
 #==========================================================
-
+# Finally, we set up a simple chat with ChatGPT.  We will have
+# to store the message and responses in the SQLlite DB so that we
+# can recall and display the chat history to the user :
 get ("/chat") do
-  erb(:chat)
+
+  erb(:chat_response)
+
 end
 
-#------------------
+#---------------------------------------------
 
 post("/chat_response") do
  
   user_message = params.fetch("user_message")
+
+  response_message = chat_response(user_message)
+
+  # Let's clean up our response message so that it can be
+  # used by the sqlite DB :
+  response_message = response_message.gsub("'", "''")
+
   
   begin
     
@@ -160,24 +202,34 @@ post("/chat_response") do
 
     insert_records = [
       "insert into chat_history values ('user', '#{user_message}')",
-      "insert into chat_history values ('assistant','answer')"
+      "insert into chat_history values ('assistant','#{response_message}')"
     ]
   
     for insert_record in insert_records
       db.execute insert_record
     end
   
-    puts "\nSelecting from both tables - ALL possible records"
-    db.execute( "select * from chat_history" ) do |row|
-       puts row
-    end
-    
+  # Just in case something crazy happens with our connection to the DB :
   rescue SQLite3::Exception => excp
-    
+  
     puts "Issue with DB : #{excp} "
     
   ensure
     db.close if db
   end
+
+  redirect("/chat")
   
 end
+
+#==========================================================
+# Let's erase the chat history and start over :
+
+post ("/clear_chat") do
+
+  set_reset_db()
+  redirect("/chat")
+
+end
+
+#==========================================================
